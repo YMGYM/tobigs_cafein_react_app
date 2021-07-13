86329d02519ba2a7caff791c482eb9f712371d18
YMGYM <good_day1234@naver.com>
2021-07-13 20:13:02 +0900

    add homepage prop

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,7 +55,7 @@ const Main = () => {
         <div>
         <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#page-top"><img width='100' src='logo_nav_black.png' /></a>
+                <a className="navbar-brand" href="#page-top">Cafe-In</a>
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ms-auto my-2 my-lg-0">
